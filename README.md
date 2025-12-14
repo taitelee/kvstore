ec2d2bdac0b9eae3597c34070bdeebdd0537679f
taitelee <taitelee@umich.edu>
2025-12-14 00:26:49 -0500

    stubs for ring, took notes on design

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 A distributed, sharded key–value store in Go. I'm going to try an implement consistent hashing, replication, rebalancing, and a WAL. Each node is going to run as an independent storage server and cooperatively forms a scalable, fault tolerant cluster capable of automatic request routing and data redistribution.
 
 This is part of the unemployed series.
+
+
+## TODO:
+* Docker setup
--- internal/hashing/ring.go
+++ internal/hashing/ring.go
@@ -6,8 +6,66 @@ import (
 	"sync"
 )
 
+// NodeID identifies a node on the ring
 type NodeID string 
 
 type Ring interface {
-	
+	AddNode(id NodeID, weight int)
+	RemoveNode(id NodeID)
+	GetPrimary(key string) NodeID
+	GetReplicas(key string, n int) []NodeID
+	Nodes() []NodeID
+}
+
+// implements the Ring interface
+type ring struct {
+	mu sync.RWMutex
+	entries []entry
+	replicas int
+}
+
+// entries are essentially virtual nodes
+type entry struct {
+    hash uint32
+    id   NodeID
+}
+
+func NewRing(replicationFactor int) Ring {
+	return &ring{
+		replicas: replicationFactor,
+		entries: make([]entry, 0),
+	}
+}
+
+// adding physical node to the ring with corresponding number of virtual nodes
+func (r *ring) AddNode(id NodeID, weight int) {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+}
+
+// remove all virtual nodes belonging to a physical node
+func (r *ring) RemoveNode(id NodeID) {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+
+}
+
+func (r *ring) GetPrimary(key string) NodeID {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+}
+
+func (r *ring) GetReplicas(key string, n int) []NodeID {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+}
+
+func (r *ring) Nodes() []NodeID {
+    r.mu.Lock()
+    defer r.mu.Unlock()
+}
+
+func hashID(s string) uint32 {
+    r.mu.Lock()
+    defer r.mu.Unlock()
 }
